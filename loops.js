d86341c0d11bf3b854428b4ab0b4af64bc8f595f
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-20 19:42:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 1; i < 25; i++) {
-    if (i == 1) {
+    if (i = 1) {
       array ="I am 1 strange loop." + array;
     } else {
       array =`I am ${i} strange loops.` + array;
