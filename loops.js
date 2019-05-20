ba37a0a0fa9e377d1b92e81e368926e30277c720
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-20 20:55:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i !== 1) {
-      array += `I am ${i} strange loops.`;
+      array.push(`I am ${i} strange loops.`);
     } else {
-      array += "I am 1 strange loop.";
+      array.push(`I am 1 strange loop.`);
     }
   }
   return array;
