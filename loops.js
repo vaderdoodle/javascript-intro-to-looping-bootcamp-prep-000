da68b26a57b5b00c3c86067d7a3321de1d2bea32
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-20 21:05:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,4 +18,15 @@ function whileLoop(number) {
 
 function doWhileLoop(integer) {
   
+  var i = 0;
+ 
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+  }
+  
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < integer); 
+  
 }
